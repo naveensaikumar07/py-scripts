174d09f6170ebe83b5af1238b07e423808e36f73
naveensaikumar007 <naveensaikumar007@gmail.com>
2024-01-04 21:42:56 +0530

    Added stop and start options

--- aws_ec2.py
+++ aws_ec2.py
@@ -7,9 +7,30 @@ print(f.renderText('AWS EC2'))
 ec2 = boto3.client('ec2')
 response = ec2.describe_instances()
 #print(response)
-
+imageId=[]
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
-        private_ip = instance['PrivateIpAddress']
-        public_ip=instance['PublicIpAddress']
-        print(f"Instance ID: {instance['InstanceId']}, PrivatIP: {private_ip} , PublicIP: {public_ip}")
+        imageId=instance['InstanceId']
+        print(f"Instance ID: {instance['InstanceId']}")
+
+action=input("Enter the action <start|stop|describe> : ")
+
+if action == 'stop':
+    response_1 = ec2.stop_instances(InstanceIds=[imageId,],)
+    print(response_1)
+elif action == 'start':
+    response_1=ec2.start_instances(InstanceIds=[imageId,],)
+    print(response_1)
+elif action == 'describe':
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            print("-"*50)
+            private_ip = instance['PrivateIpAddress']
+            if 'PublicIpAddress' in instance: 
+                public_ip=instance['PublicIpAddress']
+                print(f"Instance ID: {instance['InstanceId']},PrivatIP: {private_ip}, PublicIP: {public_ip}")
+            else:
+                print(f"Instance ID: {instance['InstanceId']}, PrivatIP: {private_ip} ")
+
+
+
