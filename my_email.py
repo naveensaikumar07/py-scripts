93ee3a0274d7a708cb678a9dc38d2292cd7a360f
naveensaikumar007 <naveensaikumar007@gmail.com>
2024-01-17 23:19:28 +0530

    updated email script

--- my_email.py
+++ my_email.py
@@ -1,36 +1,20 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import smtplib, ssl
+from email.message import EmailMessage
 
-def send_email(subject, body, to_email, smtp_server, smtp_port, smtp_username, smtp_password):
-    # Set up the MIME
-    message = MIMEMultipart()
-    message['From'] = smtp_username
-    message['To'] = to_email
-    message['Subject'] = subject
-
-    # Attach the body of the email
-    message.attach(MIMEText(body, 'plain'))
-
-    # Connect to the SMTP server
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        # Log in to the SMTP server
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-
-        # Send the email
-        server.sendmail(smtp_username, to_email, message.as_string())
-
-    print("Email sent successfully!")
-
-# Replace these values with your own
-subject = "Test Email"
-body = "This is a test email sent using Python."
-to_email = "nskumar9380@gmail.com"
+port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-smtp_port = 587
-smtp_username = "n.s.k9986431466@gmail.com"
-smtp_password = input("your_password :")
+sender_email = "test@gmail.com" #Enter your mail Addess
+receiver_email = input("Enter receiver address :") #Enter reciver mail address
+password="test" #Enter 16 digit app password
 
-# Call the function to send the email
-send_email(subject, body, to_email, smtp_server, smtp_port, smtp_username, smtp_password)
+msg = EmailMessage()
+msg.set_content("Python Test Mail")
+msg['Subject'] = "This is from Python Gmail!"
+msg['From'] = sender_email
+msg['To'] = receiver_email
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.send_message(msg, from_addr=sender_email, to_addrs=receiver_email)
+    print("Email Sent Successfully")
