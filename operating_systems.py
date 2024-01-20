ac4c7ba0e6cd3a9781800f5ab6e4787803825fe9
naveensaikumar007 <naveensaikumar007@gmail.com>
2024-01-20 20:30:34 +0530

    some os info fetching

--- operating_systems.py
+++ operating_systems.py
@@ -4,6 +4,8 @@ os.chdir("/home/nsk")
 for dirs in os.listdir():
     if  not dirs.startswith("."):
         print(dirs)
-
+print("."*50)
 print(os.system("cat /etc/os-release"))
-print(os.getcwd())
+print(os.system("uname -r "))
+print(os.system("ps -aux"))
+
