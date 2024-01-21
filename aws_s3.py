f4db86b973686bea12b9468a4678bead4519eeb8
naveensaikumar007 <naveensaikumar007@gmail.com>
2024-01-21 18:07:06 +0530

    updated s3 script to upload file to s3 bucket

--- aws_s3.py
+++ aws_s3.py
@@ -1,9 +1,30 @@
 #!/bin/python3
 import boto3
+from botocore.exceptions import NoCredentialsError
 from pyfiglet import Figlet
 f = Figlet(font='slant')
 print(f.renderText('AWS S3'))
 
 s3=boto3.client('s3')
 response=s3.list_buckets()
-print(response)
+bucket_name = response['Buckets'][0]['Name']
+print(bucket_name)
+print("="*50)
+
+def upload_to_s3(local_file, bucket_name, s3_file):
+    s3 = boto3.client('s3')
+
+    try:
+        s3.upload_file(local_file, bucket_name, s3_file)
+        print("Upload Successful")
+    except FileNotFoundError:
+        print("The file was not found")
+    except NoCredentialsError:
+        print("Credentials not available")
+
+if __name__ == "__main__":
+    local_file_path = '/home/nsk/openmetal.pub'
+    bucket_name = 'mybucketest007'
+    s3_file_path = 'test/openmetal.pub'
+
+    upload_to_s3(local_file_path, bucket_name, s3_file_path)
